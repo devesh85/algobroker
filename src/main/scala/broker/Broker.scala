603382a41a751cc0537949491c6098584f14ef3c
Devesh Kishore <devesh85>
2013-12-25 23:41:31 -0500

    Removed order def from Broker class

--- src/main/scala/broker/Broker.scala
+++ src/main/scala/broker/Broker.scala
@@ -6,13 +6,13 @@ object Broker extends App{
 	
     
     
-	trait Order {
-	    def orderId : Id
-	    def clOrderId: Id
-	    def origClOrdId : Id
-	    def orderStatus : OrderStatus
-	    def execTransType : ExecTransType.Value
-	}
+//	trait Order {
+//	    def orderId : Id
+//	    def clOrderId: Id
+//	    def origClOrdId : Id
+//	    def orderStatus : OrderStatus
+//	    def execTransType : ExecTransType.Value
+//	}
 	
 //	class ParentOrder (val orderId:OrderId) extends Order
 //    class ChildOrder (val orderId:OrderId)extends Order
